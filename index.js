404c6a6d895be7d24baa32844f61e1fa3a613f6b
Stanislav Kalashnik <sk@infomir.eu>
2015-10-09 11:59:28 +0300

    add common files

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * @module stb-gulp
+ *
+ * @author Stanislav Kalashnik <sk@infomir.eu>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+'use strict';
+
+
+
+// public
+module.exports = gulp;
