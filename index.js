2be1df4b62b50570307ea35c9263a757368d2e0a
Stanislav Kalashnik <sk@infomir.eu>
2015-10-09 12:21:43 +0300

    get task files from stb package

--- index.js
+++ index.js
@@ -7,6 +7,22 @@
 
 'use strict';
 
+var path = require('path'),
+	gulp = require('gulp'),
+	load = require('./lib/tools');
+
+
+// general app paths
+global.paths = {
+	root:   process.env.PATH_ROOT   || __dirname,
+	tasks:  process.env.PATH_TASKS  || path.join(__dirname, 'tasks'),
+	app:    process.env.PATH_APP    || path.join(__dirname, 'app'),
+	build:  process.env.PATH_BUILD  || path.join(__dirname, 'build'),
+	config: process.env.PATH_CONFIG || path.join(__dirname, 'config')
+};
+
+// load all tasks
+load(global.paths.tasks);
 
 
 // public
