79533d3efbdbd68f70cd2dba43185414c7494ee5
DarkPark <darkpark@pisem.net>
2015-12-27 22:09:00 +0200

    enable lint task

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ global.paths.config = global.paths.config || process.env.PATH_CONFIG || path.joi
 
 //require('./tasks/img');
 require('./tasks/jade');
-//require('./tasks/lint');
+require('./tasks/lint');
 //require('./tasks/repl');
 
 
