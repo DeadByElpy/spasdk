7484cc4f3ddd9de8d2b27475d0b42309e0956104
DarkPark <darkpark@pisem.net>
2015-12-28 00:39:48 +0200

    enable webpack task

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ env.PATH_CFG  = env.PATH_CFG  || path.join(env.PATH_ROOT, 'config');
 require('./tasks/jade');
 require('./tasks/lint');
 require('./tasks/repl');
+require('./tasks/webpack');
 
 
 // public
